276e82aaadf57840b96a9ad8edd8c3ada28a7fd8
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-12-11 13:33:42 +0500

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static  int n;
+        static int m;
+        static string[,] uchastok;
+
+        static void Sadovnic1()
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (uchastok[i,j]==null)
+                    {
+                        uchastok[i,j] = "Сад1";
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+
+            
+        }
+        static void Sadovnic2()
+        {
+            for (int i = n-1; i > 0; i--)           
+            {
+                for (int j = m-1; j > 0; j--)
+                {
+                    if (uchastok[j,i] == null)
+                    {
+                        uchastok[j,i] = "Сад2";
+                        Thread.Sleep(100);
+
+                    }
+                }
+            }
+
+            
+        }
+        static void Main(string[] args)
+        {
+             
+            m = (int)Convert.ToUInt32(Console.ReadLine());
+             n = (int)Convert.ToInt32(Console.ReadLine());
+
+            uchastok = new string[m, n];
+            ThreadStart threadStart = new ThreadStart(Sadovnic1);
+            Thread sad1 = new Thread(threadStart);
+            ThreadStart threadStart1 = new ThreadStart(Sadovnic2);
+            Thread sad2 = new Thread(threadStart1);
+            
+            sad1.Start();
+            sad2.Start(); 
+            sad1.Join();
+            sad2.Join();
+            
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(uchastok[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+            
+
+        }
+
+
+
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Homework2
+{
+    class Program
+    {
+        static int n;
+        static int m;
+        static int[,] uchastok;
+
+        static void Main(string[] args)
+        {
+           
+            m = (int)Convert.ToUInt32(Console.ReadLine());
+            n = (int)Convert.ToInt32(Console.ReadLine());
+
+            uchastok = new int[m-1, n-1];
+            
+            Thread sad1 = new Thread(Sadovnic1);
+            sad1.Start();
+            sad1.Join();
+             
+            for (int i = m-1; i>0; i--)
+            {
+                for (int j = n-1; j >0; j--)
+                {
+                    if (uchastok[i, j] == 0)
+                    {
+                        uchastok[i, j] = 1;
+                    }
+                    Console.WriteLine(uchastok[i, j]);
+                }
+            }
+            Console.ReadKey();
+        }
+
+ 
+        static void Sadovnic1()
+        {
+            for (int i = 0; i <n; i++)
+            {
+                for (int j = 0; j <m; j++)
+                {
+                    if (uchastok[i, j] == 0)
+                    {
+                        uchastok[i, j] = 2;
+                    }
+                }
+            }
+            
+
+        }
+        
+    }
+}
